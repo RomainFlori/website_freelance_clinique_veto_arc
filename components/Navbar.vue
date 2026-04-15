de6e8cb23f00c16a23d9508ab30670e58da95c86
Romain Flori-Cantrelle <romainflori@orange.fr>
2026-04-15 11:18:44 +0200

    change logo size

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,8 +6,8 @@
     <nav class="bg-white sticky top-0 z-50">
       <div class="max-w-7xl mx-auto px-4 flex items-center justify-between">
         <div class="flex">
-          <a href="#">
-            <NuxtImg class="block h-full w-full object-cover" src="/logo.png" sizes="lg:150px" format="webp" />
+          <a href="\">
+            <NuxtImg class="block h-full w-full object-cover" src="/logo.png" sizes="90px" format="webp" />
           </a>
           <!-- <div class="font-bold text-emerald-600 text-xl">Clinique vétérinaire de l'arc</div> -->
 
